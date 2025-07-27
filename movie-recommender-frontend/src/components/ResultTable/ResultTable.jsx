16c2acbf1b1ec46be818c3fb9542b673f86e2661
Prakash Gupta <prakash.25gupta@gmail.com>
2025-07-27 20:41:37 +0530

    Updated table responsiveness

--- movie-recommender-frontend/src/components/ResultTable/ResultTable.jsx
+++ movie-recommender-frontend/src/components/ResultTable/ResultTable.jsx
@@ -4,8 +4,6 @@ import './ResultTable.css';
 const ITEMS_PER_PAGE = 5;
 
 const ResultTable = ({ data, type }) => {
-    console.log("data:", data);
-    
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPages = useMemo(() => {
@@ -24,28 +22,37 @@ const ResultTable = ({ data, type }) => {
 
   return (
     <div className="result__container">
-      <table className="result__table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genres</th>
-            <th>Cast</th>
-            <th>Year</th>
-            <th>Similarity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {paginatedData.map((movie, index) => (
-            <tr key={index}>
-              <td>{movie.title}</td>
-              <td>{movie.genres?.join(', ')}</td>
-              <td>{movie.cast?.slice(0, 3).join(', ')}</td>
-              <td>{movie.release_year}</td>
-              <td>{movie.similarity}%</td>
+      <div className="table__scroll-wrapper">
+        <table className="result__table">
+          <colgroup>
+            <col style={{ width: '25%' }} />
+            <col style={{ width: '20%' }} />
+            <col style={{ width: '25%' }} />
+            <col style={{ width: '15%' }} />
+            <col style={{ width: '15%' }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genres</th>
+              <th>Cast</th>
+              <th>Year</th>
+              <th>Similarity</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {paginatedData.map((movie, index) => (
+              <tr key={index}>
+                <td>{movie.title}</td>
+                <td>{movie.genres?.join(', ')}</td>
+                <td>{movie.cast?.slice(0, 3).join(', ')}</td>
+                <td>{movie.release_year}</td>
+                <td>{movie.similarity}%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <div className="pagination__controls">
         <button
